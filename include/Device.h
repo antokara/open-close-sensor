fc961ab16246700cc2596f8f8069c230521f5b75
Antonios Karagiannis <antokarag@gmail.com>
2025-12-26 14:52:12 -0700

    add calibration mode

--- include/Device.h
+++ include/Device.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include "Adafruit_FreeTouch.h"
 
 /**
  * has fast to be blinking the LED,
@@ -16,7 +17,23 @@
 /**
  * 0 = no debug messages, higher numbers = more verbose
  */
-#define DEBUG_LEVEL DEBUG_LEVEL_INFO
+#define DEBUG_LEVEL DEBUG_LEVEL_VERBOSE
+
+/**
+ * pin used for calibration input (e.g. button or touch sensor)
+ */
+#define CALIBRATION_INPUT_PIN A0
+
+/**
+ * touch threshold for capacitive input
+ */
+#define TOUCH_THRESHOLD 800
+
+/**
+ * time in ms the calibration input must be held
+ * to enter calibration mode
+ */
+#define CALIBRATION_HOLD_TIME_MS 2000
 
 enum Debug_Level
 {
@@ -38,6 +55,35 @@ private:
      * timestamp of last led toggle
      */
     uint32_t last_led_toggle_{0};
+
+    /**
+     * true if device is in calibration mode
+     */
+    bool is_calibrating_{false};
+
+    /**
+     * Adafruit_FreeTouch instance for the calibration input
+     */
+    Adafruit_FreeTouch qtCalibration_;
+
+    /**
+     * check if calibration button is pressed
+     */
+    void check_calibration_button();
+
+    /**
+     * timestamp when calibration button was first pressed
+     *
+     * 0 if not currently pressed
+     * -1 if pressed and toggled but not yet released
+     */
+    u_int32_t calibration_button_pressed_time_{0};
+
+    /**
+     * boolean flag indicating if calibration was toggled
+     * since the last button press, to prevent retriggering
+     */
+    bool calibration_toggled_{false};
 
 public:
     Device();
@@ -68,4 +114,14 @@ public:
      * log message to serial if level >= DEBUG_LEVEL
      */
     void log(const String &message, bool ln = true, Debug_Level level = DEBUG_LEVEL_INFO);
+
+    /**
+     * start calibration mode
+     */
+    void start_calibration();
+
+    /**
+     * stop calibration mode
+     */
+    void stop_calibration();
 };
--- src/Device.cpp
+++ src/Device.cpp
@@ -4,13 +4,22 @@ Device::Device() {}
 
 void Device::setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
 
     // Start serial communication at 115200 baud
     Serial.begin(115200);
 
     // Wait for serial monitor to open
     delay(SETUP_DELAY_MS);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // setup the calibration input pin
+    this->qtCalibration_ = Adafruit_FreeTouch(CALIBRATION_INPUT_PIN, OVERSAMPLE_4, RESISTOR_0, FREQ_MODE_NONE);
+
+    if (!this->qtCalibration_.begin())
+    {
+        this->log("Failed to start touch sensor for the Calibration Input", true, DEBUG_LEVEL_ERROR);
+    }
 }
 
 void Device::loop()
@@ -24,6 +33,7 @@ void Device::loop()
             this->last_led_toggle_ = now;
         }
     }
+    this->check_calibration_button();
 }
 
 bool Device::has_fault()
@@ -36,17 +46,66 @@ void Device::set_fault(bool fault)
     this->has_fault_ = fault;
     if (!fault)
     {
-        digitalWrite(LED_BUILTIN, LOW);
+        digitalWrite(LED_BUILTIN, HIGH);
     }
 }
 
 void Device::log(const String &message, bool ln, Debug_Level level)
 {
-    if (level >= DEBUG_LEVEL)
+    if (level <= DEBUG_LEVEL)
     {
         if (ln)
             Serial.println(message);
         else
             Serial.print(message);
+    }
+}
+
+void Device::start_calibration()
+{
+    is_calibrating_ = true;
+    digitalWrite(LED_BUILTIN, LOW);
+    this->log("Entered calibration mode", true, DEBUG_LEVEL_INFO);
+}
+
+void Device::stop_calibration()
+{
+    is_calibrating_ = false;
+    digitalWrite(LED_BUILTIN, HIGH);
+    this->log("Exited calibration mode", true, DEBUG_LEVEL_INFO);
+}
+
+void Device::check_calibration_button()
+{
+    if (this->qtCalibration_.measure() >= TOUCH_THRESHOLD)
+    {
+        if (this->calibration_button_pressed_time_ != 0 && !this->calibration_toggled_)
+        {
+            // button held down
+            if (millis() - this->calibration_button_pressed_time_ >= CALIBRATION_HOLD_TIME_MS)
+            {
+                // long enough to toggle calibration state
+                if (!this->is_calibrating_)
+                {
+                    this->start_calibration();
+                }
+                else
+                {
+                    this->stop_calibration();
+                }
+                this->calibration_toggled_ = true;
+            }
+        }
+        else if (this->calibration_button_pressed_time_ == 0)
+        {
+            // first press
+            this->calibration_button_pressed_time_ = millis();
+        }
+    }
+    else if (this->calibration_button_pressed_time_ != 0)
+    {
+        // not pressed
+        this->calibration_button_pressed_time_ = 0;
+        this->calibration_toggled_ = false;
     }
 }
